e09e4452493594701815351fd57bd599df8a69ff
Rasmus Praestholm <cervator@gmail.com>
2013-07-06 00:00:54 -0400

    Sample Android facade using LibGDX

---
+++ src/com/example/mylibgdxgame/MyLibgdxGameAndroidStarter.java
@@ -0,0 +1,21 @@
+package com.example.mylibgdxgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MyLibgdxGameAndroidStarter extends AndroidApplication
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
+        cfg.useWakelock = true;
+        cfg.useGL20 = true;
+        initialize(new MyLibgdxGame(), cfg);
+    }
+}
